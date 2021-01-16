0a0ac8feb4b22824d582b3abee3858a06ab17595
Welton Vitor da S. Jesus <wvitorsj08@gmail.com>
2021-01-16 11:49:56 -0300

    commit inicial

---
+++ Erro03.java
@@ -0,0 +1,12 @@
+Class Erro03{
+
+	public static void Main(String[] args){
+
+		System.out.println("Olá Mundo " );
+	}
+}
+
+//Erros em Tempos de Execução....que ocorrem devido a erros no algoritimo
+
+/*O Java é case sensitive, ou seja, 
+ele difere letras Maiúsculas de letras Minúsculas.*/
